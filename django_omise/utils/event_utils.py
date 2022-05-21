d1dbf201170f15ae225b2b062489d03b7bb55f35
Siwat Tansiri <tansirijames@gmail.com>
2022-05-21 12:40:48 +0700

    feat: Handle schedule event

--- django_omise/utils/event_utils.py
+++ django_omise/utils/event_utils.py
@@ -2,12 +2,18 @@ from django_omise.models.event import EventType, Event
 from django_omise.models.core import Charge
 from django_omise.omise import omise
 
+from django_omise.utils.core_utils import update_or_create_from_omise_object
 
-def pre_event_handle(omise_event: omise.Event):
+
+from typing import Dict
+
+
+def pre_event_handle(omise_event: omise.Event, raw_event: Dict):
     """
     Perform additional actions on Omise Event object before handling with the webhook view.
 
     :params omise_event: The Omise Event object
+    :param raw_event: The event data as received with webhook view
 
     :returns: None
     """
@@ -15,13 +21,36 @@ def pre_event_handle(omise_event: omise.Event):
         charge = Charge.objects.get(pk=omise_event.data.charge)
         charge.reload_from_omise()
 
+    if omise_event.key.startswith("schedule."):
+        update_or_create_from_omise_object(
+            omise_object=omise_event.data,
+            ignore_fields=[
+                "occurrences",
+            ],
+        )
 
-def post_event_handle(omise_event: omise.Event, event_object: Event):
+
+def post_event_handle(omise_event: omise.Event, event_object: Event, raw_event: Dict):
     """
     Perform additional actions on Omise Event object after handling with the webhook view.
 
     :params omise_event: The Omise Event object
     :param event_object: The new event object in the database
+    :param raw_event: The event data as received with webhook view
 
     :returns: None
     """
+
+    if omise_event.key.startswith("charge."):
+
+        schedule_id = raw_event.get("data", {}).get("schedule", None)
+        omise_schedule = None
+        schedule = None
+
+        if schedule_id:
+            omise_schedule = omise.Schedule.retrieve(schedule_id)
+            schedule = update_or_create_from_omise_object(omise_object=omise_schedule)
+
+        charge = event_object.event_object
+        charge.schedule = schedule
+        charge.save()
