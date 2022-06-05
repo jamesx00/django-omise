b7deaec81d390750491183ae73e0fd5d57f1698c
Siwat Tansiri <tansirijames@gmail.com>
2022-06-05 12:15:31 +0200

    refactor: Move MockResponse as a separate class

--- django_omise/tests/test_utils.py
+++ django_omise/tests/test_utils.py
@@ -1,35 +1,41 @@
 import json
 
-from .mockdata.customer import customer_response
+from .mockdata.customer import customer_response, add_card_response
+from .mockdata.token import token_response
+
+
+class MockResponse:
+    def __init__(self, raw_response, status_code):
+        self.status_code = status_code
+        self.raw_response = raw_response
+
+    def json(self):
+        return json.loads(self.raw_response)
 
 
 def mocked_requests_post(*args, **kwargs):
-    class MockResponse:
-        def __init__(self, raw_response, status_code):
-            self.status_code = status_code
-            self.raw_response = raw_response
 
-        def json(self):
-            return json.loads(self.raw_response)
+    request_url = args[0]
 
-    if args[0] == "https://api.omise.co/customers":
+    if request_url == "https://api.omise.co/customers":  # omise.Customer.create
+        return MockResponse(customer_response, 200)
+
+    if request_url == "https://vault.omise.co/tokens":  # omise.Token.create
         return MockResponse(customer_response, 200)
 
     return MockResponse(None, 404)
 
 
 def mocked_requests_get(*args, **kwargs):
-    class MockResponse:
-        def __init__(self, raw_response, status_code):
-            self.status_code = status_code
-            self.raw_response = raw_response
-
-        def json(self):
-            return json.loads(self.raw_response)
 
     request_url = args[0]
 
-    if "https://api.omise.co/customers" in request_url:
+    if "https://api.omise.co/customers" in request_url:  # omise.Customer.retrieve
         return MockResponse(customer_response, 200)
+
+    if (
+        request_url == "https://vault.omise.co/tokens/test_token_id"
+    ):  # omise.Token.retrieve
+        return MockResponse(token_response, 200)
 
     return MockResponse(None, 404)
