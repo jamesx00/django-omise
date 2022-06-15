b670b5e105f1029e472d291c1cdcfc03b06fce2c
Siwat Tansiri <tansirijames@gmail.com>
2022-06-15 18:09:46 +0700

    chore: Cleanup class imports

--- django_omise/admin/__init__.py
+++ django_omise/admin/__init__.py
@@ -1,1 +1,16 @@
-from .admin import *
+from .admin import (
+    CardInline,
+    ChargeScheduleInline,
+    ChargeInline,
+    CustomerAdmin,
+    CardAdmin,
+    RefundAdmin,
+    EventAdmin,
+    RefundInline,
+    ChargeAdmin,
+    SourceAdmin,
+    OccurrenceInline,
+    ScheduleAdmin,
+    OccurrenceAdmin,
+    ChargeScheduleAdmin,
+)
