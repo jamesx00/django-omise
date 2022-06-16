1d9b416c939163a3c0bf1913d4cfcc1eca724c55
Siwat Tansiri <tansirijames@gmail.com>
2022-06-16 21:58:40 +0700

    test: Add settings for pytest

---
+++ django_omise/tests/settings.py
@@ -0,0 +1,48 @@
+DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+    }
+}
+
+ROOT_URLCONF = "urls"
+USE_TZ = True
+TIME_ZONE = "Asia/Bangkok"
+
+INSTALLED_APPS = (
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.admin",
+    "django_omise",
+)
+
+SECRET_KEY = "django_tests_secret_key"
+OMISE_PUBLIC_KEY = "test_omise_public_key"
+OMISE_SECRET_KEY = "test_omise_secret_key"
+OMISE_LIVE_MODE = False
+MIDDLEWARE = (
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+)
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
