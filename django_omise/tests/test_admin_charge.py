6947012bbc3a838519bade5ff64e0df385abca27
Siwat Tansiri <tansirijames@gmail.com>
2022-06-16 14:08:17 +0700

    test: Add tests for chargeinline

--- django_omise/tests/test_admin_charge.py
+++ django_omise/tests/test_admin_charge.py
@@ -1,7 +1,7 @@
 from django.contrib.admin.sites import AdminSite
 from django.contrib.auth import get_user_model
 
-from django_omise.admin import ChargeAdmin
+from django_omise.admin import ChargeAdmin, ChargeInline
 from django_omise.models.core import Charge
 from django_omise.models.choices import Currency, ChargeStatus
 
@@ -54,6 +54,53 @@ class AdminTestCase(ClientAndUserBaseTestCase):
         )
         charge_admin.colorized_status(obj=charge)
         mock_format_html.assert_called_once()
+
+    @mock.patch("django_omise.admin.admin.format_html")
+    def test_charge_inline_colorized_status_successful(self, mock_format_html):
+        charge_inline = ChargeInline(parent_model=Charge, admin_site=AdminSite())
+
+        charge = self.create_charge(
+            status=ChargeStatus.SUCCESSFUL,
+        )
+        charge_inline.colorized_status(obj=charge)
+        mock_format_html.assert_called_once()
+
+    @mock.patch("django_omise.admin.admin.format_html")
+    def test_charge_inline_colorized_status_failed(self, mock_format_html):
+        charge_inline = ChargeInline(parent_model=Charge, admin_site=AdminSite())
+
+        charge = self.create_charge(
+            status=ChargeStatus.FAILED,
+        )
+        charge_inline.colorized_status(obj=charge)
+        mock_format_html.assert_called_once()
+
+    @mock.patch("django_omise.admin.admin.format_html")
+    def test_charge_inline_colorized_status_pending(self, mock_format_html):
+        charge_inline = ChargeInline(parent_model=Charge, admin_site=AdminSite())
+
+        charge = self.create_charge(
+            status=ChargeStatus.PENDING,
+        )
+        charge_inline.colorized_status(obj=charge)
+        mock_format_html.assert_called_once()
+
+    @mock.patch("django_omise.admin.admin.format_html")
+    def test_charge_inline_colorized_status_refunded(self, mock_format_html):
+        charge_inline = ChargeInline(parent_model=Charge, admin_site=AdminSite())
+
+        charge = self.create_charge(
+            status=ChargeStatus.SUCCESSFUL,
+            refunded_amount=1000,
+        )
+        charge_inline.colorized_status(obj=charge)
+        mock_format_html.assert_called_once()
+
+    def test_charge_inline_change_permission(self):
+        charge_inline = ChargeInline(parent_model=Charge, admin_site=AdminSite())
+        request = MockRequest(user=self.user)
+
+        self.assertEqual(charge_inline.has_change_permission(request=request), False)
 
     def create_charge(self, **kwargs):
         default = {
