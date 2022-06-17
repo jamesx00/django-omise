ce48ecc6c5d2d9a59105343a1e2fa70ccc2694b4
Siwat Tansiri <tansirijames@gmail.com>
2022-06-17 09:27:55 +0700

    chore: Rename files

--- django_omise/tests/test_event.py
+++ django_omise/tests/test_event.py
@@ -1,9 +1,4 @@
-from django.test import TestCase
-
-import code
-import readline
-
-from django_omise.models.schedule import Schedule, Occurrence
+from django_omise.models.schedule import Schedule
 from django_omise.omise import omise
 from django_omise.utils.core_utils import update_or_create_from_omise_object
 
@@ -14,13 +9,13 @@ from unittest import mock
 from .test_utils import mocked_schedule_event_request
 
 # Create your tests here.
-class WebhookTestCase(OmiseBaseTestCase):
+class EventTestCase(OmiseBaseTestCase):
     def setUp(self):
         self.customer = self.create_customer(id="test_customer_id")
         self.card = self.create_card(id="test_card_id")
 
     @mock.patch("requests.get", side_effect=mocked_schedule_event_request)
-    def test_schedule_webhook(self, mock_schedule_event):
+    def test_schedule_event(self, mock_schedule_event):
         event = omise.Event.retrieve("test_event_id")
         event_data = event.data
         schedule = update_or_create_from_omise_object(omise_object=event_data)
